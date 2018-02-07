12d75a74be2b347bdc84653a989636d566a95cd7
Andrey Grachev <andreysgra@gmail.com>
2018-02-07 15:25:25 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -1653,46 +1653,64 @@
 
 (function () {
   var HEADER_SEL = '.page-header';
-  var NO_JS_CLS = 'page-header--no-js';
-  var MENU_CLOSED_CLS = 'page-header--menu-closed';
-  var BTN_TOGGLE_CLS = 'page-header__toggle';
-  var BTN_TOGGLE_OFF_CLS = 'page-header__toggle--off';
+  var NAV_SEL = '.main-nav';
+  var NO_JS_CLS = 'main-nav--no-js';
+  var HEADER_CLOSED_CLS = 'page-header--nav-closed';
+  var NAV_CLOSED_CLS = 'main-nav--closed';
+  var BTN_TOGGLE_CLS = 'main-nav__toggle';
+  var BTN_TOGGLE_OFF_CLS = 'main-nav__toggle--off';
 
   var pageHeader = document.querySelector(HEADER_SEL);
+  var mainNav = document.querySelector(NAV_SEL);
 
-  if (pageHeader) {
-    if (pageHeader.classList.contains(NO_JS_CLS)) {
-      pageHeader.classList.remove(NO_JS_CLS);
-      pageHeader.classList.add(MENU_CLOSED_CLS);
+  if (mainNav && mainNav.classList.contains(NO_JS_CLS)) {
+    mainNav.classList.remove(NO_JS_CLS);
+    mainNav.classList.add(NAV_CLOSED_CLS);
+
+    if (pageHeader) {
+      pageHeader.classList.add(HEADER_CLOSED_CLS);
     }
   }
 
-  pageHeader.addEventListener('click', function (event) {
+  mainNav.addEventListener('click', function (event) {
     if (event.target.classList.contains(BTN_TOGGLE_CLS)) {
       event.preventDefault();
 
-      this.classList.toggle(MENU_CLOSED_CLS);
+      this.classList.toggle(NAV_CLOSED_CLS);
       event.target.classList.toggle(BTN_TOGGLE_OFF_CLS);
+      pageHeader.classList.toggle(HEADER_CLOSED_CLS);
     }
-  })
+  });
+
 })();
 
 // Яндекс карта
 function init (ymaps) {
   var map = new ymaps.Map('map', {
-    center: [59.936800, 30.322861],
-    zoom: 15,
-    controls: ['zoomControl']
+    center: [59.93863106417265, 30.3230545],
+    zoom: 16,
+    controls: []
   });
 
-  var placemark = new ymaps.Placemark([59.936160, 30.322861], {}, {
+  var placemark = new ymaps.Placemark([59.93863106417265, 30.3230545], {}, {
     iconLayout: 'default#image',
     iconImageHref: 'img/icon-map-marker.svg',
-    iconImageSize: [35, 35]
+    iconImageSize: [35, 35],
+    iconImageOffset: [-18, -18]
+  });
+
+  var zoomControl = new ymaps.control.ZoomControl({
+    options: {
+      position: {
+        left: 10,
+        bottom: 50
+      }
+    }
   });
 
   map.behaviors.disable('scrollZoom');
   map.geoObjects.add(placemark);
+  map.controls.add(zoomControl);
 }
 
 svg4everybody();
